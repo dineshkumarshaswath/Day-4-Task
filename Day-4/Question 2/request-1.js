0ab85f41cb36f380b5cf5f5869cc7263ac31cce1
DINESHKUMAR R <124970543+dineshkumarshaswath@users.noreply.github.com>
2023-03-06 19:44:47 +0530

    Add files via upload

---
+++ Day-4/Question 2/request-1.js
@@ -0,0 +1,15 @@
+//2.  Use the rest country's API URL ->https://restcountries.com/v2/all and display all the 
+//country flags in the console.
+
+var request= new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+     var result=JSON.parse(request.response);
+    console.log(result);
+
+var res=result;
+for(var i=0;i<result.length;i++){
+    console.log(result[i].flags);
+}
+}
